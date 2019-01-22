2e4fd6c794fe29c676fa5c60e6ed293c9419d3db
Yunato <yunato525@gmail.com>
2019-01-22 15:29:45 +0900

    Add Game (DTO)

---
+++ app/src/main/java/io/github/yunato/judotimer/model/Game.kt
@@ -0,0 +1,6 @@
+package io.github.yunato.judotimer.model
+
+data class Game(val num: Int,
+                val grade: String,
+                val title: String,
+                val subtitle: String)
