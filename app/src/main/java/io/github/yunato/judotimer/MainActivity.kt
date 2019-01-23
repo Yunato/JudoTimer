1df427dcd8364aa10bef76ac365e92bb41852757
Yunato <yunato525@gmail.com>
2019-01-23 16:57:46 +0900

    Create UI (Standard)

--- app/src/main/java/io/github/yunato/judotimer/MainActivity.kt
+++ app/src/main/java/io/github/yunato/judotimer/MainActivity.kt
@@ -2,11 +2,20 @@ package io.github.yunato.judotimer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    val TAG: String = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById(R.id.start_button) as Button
+
+        startButton.setOnClickListener {
+            Log.d(TAG, "START")
+        }
     }
 }
