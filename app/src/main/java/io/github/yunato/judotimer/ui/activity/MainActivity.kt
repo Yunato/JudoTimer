fc3299494de6f871b2a9922410776e682ec56121
Yunato <yunato525@gmail.com>
2019-01-28 00:15:45 +0900

    Apply tablet terminal on MainActivity

--- app/src/main/java/io/github/yunato/judotimer/ui/activity/MainActivity.kt
+++ app/src/main/java/io/github/yunato/judotimer/ui/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package io.github.yunato.judotimer.ui.activity
 
+import android.content.pm.ActivityInfo
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import io.github.yunato.judotimer.R
@@ -13,6 +14,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(resources.getBoolean(R.bool.portrait_only)){
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }else{
+            requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
 
         val transition = fragmentManager.beginTransaction()
         val fragment = InputGameInfoFragment.newInstance()
