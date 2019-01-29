c66db6f8ec94bf764d0971452371b70590523a03
Yunato <yunato525@gmail.com>
2019-01-29 23:15:21 +0900

    Implement Parcelable into DTO

--- app/src/main/java/io/github/yunato/judotimer/model/dto/Game.kt
+++ app/src/main/java/io/github/yunato/judotimer/model/dto/Game.kt
@@ -1,7 +1,34 @@
 package io.github.yunato.judotimer.model.dto
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Game(val num: Int,
                 val name: String,
                 val grade: String,
                 val rank: String,
-                val subtitle: String)
+                val subtitle: String) : Parcelable {
+
+    companion object{
+        @JvmField
+        val CREATOR: Parcelable.Creator<Game> = object :Parcelable.Creator<Game>{
+            override fun createFromParcel(source: Parcel): Game = source.run {
+                Game(readInt(), readString(), readString(), readString(), readString())
+            }
+
+            override fun newArray(size: Int): Array<Game?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run{
+            writeInt(num)
+            writeString(name)
+            writeString(grade)
+            writeString(rank)
+            writeString(subtitle)
+        }
+    }
+}
--- app/src/main/java/io/github/yunato/judotimer/model/dto/Player.kt
+++ app/src/main/java/io/github/yunato/judotimer/model/dto/Player.kt
@@ -1,4 +1,28 @@
 package io.github.yunato.judotimer.model.dto
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Player(val name: String,
-                  val belongs: String)
+                  val belongs: String):Parcelable {
+
+    companion object {
+        @JvmField
+        val CREATOR: Parcelable.Creator<Player> = object : Parcelable.Creator<Player> {
+            override fun createFromParcel(source: Parcel): Player = source.run {
+                Player(readString(), readString())
+            }
+
+            override fun newArray(size: Int): Array<Player?> = arrayOfNulls(size)
+        }
+    }
+
+    override fun describeContents(): Int = 0
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.run {
+            writeString(name)
+            writeString(belongs)
+        }
+    }
+}
